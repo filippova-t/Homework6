6a7e76015dcfaba195d65cc49877f018614a7dbe
filippova_t <tanya-fef@yandex.ru>
2022-07-27 01:58:12 +0800

    домашка 6 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] costs = generateRandomArray();
+        //Задение 1
+        int sum = 0;
+        for (int i = 0; i < costs.length; i++) {
+            sum += costs[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+        //Задание 2
+        int maxCost = 99_999;
+        for (final int current : costs) {
+            if (current > maxCost) {
+                maxCost = current;
+            }
+        }
+        int minCost = 200_001;
+        for (final int current : costs) {
+            if (current < minCost) {
+                minCost = current;
+            }
+        }
+        System.out.print("Минимальная сумма трат за день составила " +  minCost + " рублей. Максимальная сумма трат ");
+        System.out.println("за день составила " + maxCost + " рублей");
+
+        //Задание 3
+        double averageCost = (double) sum / 30;
+        String averageCostRounded = String.format("%.2f",averageCost);
+        System.out.println("Средняя сумма трат за месяц составила " + averageCostRounded + " рублей");
+
+        //Задание 4
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.println(reverseFullName[i]);
+                break;
+            }
+            System.out.print(reverseFullName[i] + " ");
+
+        }
+
+
+    }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] costs = new int[30];
+        for (int i = 0; i < costs.length; i++) {
+            costs [i] = random.nextInt(100_000) + 100_000;
+        }
+        return costs;
     }
 }
